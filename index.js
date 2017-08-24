8da249405594032ad77ddbe5177b35ffc2eddc2e
Avi <avirzajac@gmail.com>
2017-08-23 20:41:47 -0400

    Quit button made, WIP console.log on click

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const {app, Menu, Tray} = require('electron')
 
-let tray = null
-app.on('ready', () => {
-  tray = new Tray('./images/tray_icon_purple.png')
-  const contextMenu = Menu.buildFromTemplate([
-    {label: 'Item1', type: 'radio'},
-    {label: 'Item2', type: 'radio'},
-    {label: 'Item3', type: 'radio', checked: true},
-    {label: 'Item4', type: 'radio'}
-  ])
-  tray.setToolTip('This is my application.')
-  tray.setContextMenu(contextMenu)
-})
+// let tray = null
+// app.on('ready', () => {
+//   tray = new Tray('./images/tray_icon_purple.png')
+//   const contextMenu = Menu.buildFromTemplate([
+//     {label: 'Item1', type: 'radio'},
+//     {label: 'Item2', type: 'radio'},
+//     {label: 'Item3', type: 'radio', checked: true},
+//     {label: 'Item4', type: 'radio'}
+//   ])
+//   tray.setToolTip('This is my application.')
+//   tray.setContextMenu(contextMenu)
+// })
+
+// // This is for Linux distros that have issues!
 let appIcon = null
 app.on('ready', () => {
   appIcon = new Tray('./images/tray_icon_purple.png')
   const contextMenu = Menu.buildFromTemplate([
     {label: 'Item1', type: 'radio'},
-    {label: 'Item2', type: 'radio'}
+    {label: 'Item2', type: 'radio'},
+    {label: 'Quit', type: 'radio'},
+    {label: 'Click me', click() {console.log('clicked the button')}, type: 'radio'}
   ])
 
   // Make a change to the context menu
